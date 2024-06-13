ff47563c54537497743e85f63ff69431a5dc3373
KauaHenSilva <kaua.sbc@gmail.com>
2024-06-13 15:38:00 -0300

    feat: Add TransactionsList and TransactionsModel classes

---
+++ lib/models/transactions_model.dart
@@ -0,0 +1,11 @@
+class TransactionsModel {
+  final String title;
+  final String date;
+  final String amount;
+
+  TransactionsModel({
+    required this.title,
+    required this.date,
+    required this.amount,
+  });
+}

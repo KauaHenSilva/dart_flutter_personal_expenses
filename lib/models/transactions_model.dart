7548beb8b712779d1b518542ba52029c877c515c
KauaHenSilva <kaua.sbc@gmail.com>
2024-06-13 17:54:55 -0300

    feat: Add id field to TransactionsModel

--- lib/models/transactions_model.dart
+++ lib/models/transactions_model.dart
@@ -1,9 +1,11 @@
 class TransactionsModel {
+  final String id;
   final String title;
   final DateTime date;
   final String amount;
 
   TransactionsModel({
+    required this.id,
     required this.title,
     required this.date,
     required this.amount,
